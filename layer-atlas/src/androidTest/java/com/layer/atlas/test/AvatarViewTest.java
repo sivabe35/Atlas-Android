db8c63619889a944a2134346603d5780ddec4401
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-07-05 16:59:09 +0100

    Write test for Avatar and presence

---
+++ layer-atlas/src/androidTest/java/com/layer/atlas/test/AvatarViewTest.java
@@ -0,0 +1,41 @@
+package com.layer.atlas.test;
+
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.layer.atlas.view.AvatarActivityTestView;
+import com.layer.ui.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AvatarViewTest {
+
+    @Rule
+    public ActivityTestRule<AvatarActivityTestView> mNotesActivityTestRule =
+            new ActivityTestRule<>(AvatarActivityTestView.class);
+
+    @Test
+    public void testThatAvatarColorChangeWhenSpinnerIsChanged() {
+        String selectionText = "away";
+
+        onView(withId(R.id.test_spinner)).perform(click());
+        //onData(allOf(is(instanceOf(String.class)), is(selectionText))).perform(click());
+        //onView(withId(R.id.test_spinner)).check(matches(withSpinnerText(containsString(selectionText))));
+    }
+
+
+}
--- layer-atlas/src/androidTest/java/com/layer/atlas/view/AvatarActivityTestView.java
+++ layer-atlas/src/androidTest/java/com/layer/atlas/view/AvatarActivityTestView.java
@@ -2,9 +2,14 @@ package com.layer.atlas.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.layer.atlas.mock.MockLayerClient;
 import com.layer.sdk.LayerClient;
+import com.layer.sdk.messaging.Presence;
 import com.layer.ui.R;
 import com.layer.ui.avatar.AvatarView;
 import com.layer.ui.avatar.AvatarViewModel;
@@ -12,18 +17,57 @@ import com.layer.ui.util.imagecache.ImageCacheWrapper;
 import com.layer.ui.util.imagecache.PicassoImageCacheWrapper;
 import com.layer.ui.util.imagecache.requesthandlers.MessagePartRequestHandler;
 
-public class AvatarActivityTestView extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvatarActivityTestView extends Activity implements AdapterView.OnItemSelectedListener{
 
     private AvatarView mAvatarView;
+    private Spinner mPresenceSpinner;
+    private ArrayAdapter<String> mPresenceSpinnerDataAdapter;
+    private LayerClient mLayerClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avatar_test);
-        LayerClient layerClient = new MockLayerClient();
+        mLayerClient = new MockLayerClient();
         mAvatarView = (AvatarView) findViewById(R.id.test_avatar);
-        MessagePartRequestHandler messagePartRequestHandler = new MessagePartRequestHandler(layerClient);
+        mPresenceSpinner = (Spinner) findViewById(R.id.test_spinner);
+        MessagePartRequestHandler messagePartRequestHandler = new MessagePartRequestHandler(mLayerClient);
         ImageCacheWrapper imageCacheWrapper = new PicassoImageCacheWrapper(messagePartRequestHandler, this);
         mAvatarView.init(new AvatarViewModel(imageCacheWrapper));
+        setUp();
+    }
+
+    private void setUp() {
+        mPresenceSpinner.setOnItemSelectedListener(this);
+        List<String> presenceStates = new ArrayList<>();
+        for (Presence.PresenceStatus status : Presence.PresenceStatus.values()) {
+            if (status.isUserSettable()) {
+                presenceStates.add(status.toString());
+            }
+        }
+        mPresenceSpinnerDataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, presenceStates);
+        mPresenceSpinnerDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mPresenceSpinner.setAdapter(mPresenceSpinnerDataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (!mLayerClient.isAuthenticated()) return;
+
+        String newSelection = mPresenceSpinnerDataAdapter.getItem(i).toString();
+        Presence.PresenceStatus newStatus = Presence.PresenceStatus.valueOf(newSelection);
+        if (mLayerClient.isAuthenticated()) {
+            mLayerClient.setPresenceStatus(newStatus);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
