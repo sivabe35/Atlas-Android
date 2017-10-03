04f3050b01dbd1e259e3fb2d487da397a6244ea0
Archit Joshi <archit@layer.com>
2017-10-02 19:53:03 -0700

    Basic test class

---
+++ layer-atlas/src/test/java/com/layer/ui/messageitem/BindingRegistryTest.java
@@ -0,0 +1,25 @@
+package com.layer.ui.messageitem;
+
+import com.layer.sdk.LayerClient;
+import com.layer.sdk.messaging.Message;
+import com.layer.sdk.messaging.MessagePart;
+
+import org.junit.Before;
+import org.mockito.Mock;
+
+public class BindingRegistryTest {
+
+    @Mock
+    LayerClient layerClient;
+
+    @Mock
+    Message message;
+
+    @Mock
+    MessagePart messagePart;
+
+    @Before
+    public void setup() {
+
+    }
+}
